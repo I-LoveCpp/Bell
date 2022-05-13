e565b90a3086a24a0aab5f0fa17c6fa83f450f86
I-LoveCpp <90536015+I-LoveCpp@users.noreply.github.com>
2022-05-13 10:25:02 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-#define edition "Bell 1.2.1"
+#define edition "Bell 1.3.0"
 
 SYSTEMTIME sys;
 #define hour sys.wHour
@@ -21,6 +21,8 @@ SYSTEMTIME sys;
 #include "begin.rc"
 #include "end.rc"
 #include "exercise.rc"
+
+int mode = 1;
 
 void playsound(string Music)
 {
@@ -33,11 +35,24 @@ void delete_file()
 	DeleteFile(_T("上课.mp3"));
 	DeleteFile(_T("下课.mp3"));
 	DeleteFile(_T("眼保健操.mp3"));
+	ofstream out("D:\\Data\\Bell\\Mode.Ceh", ios::binary);
+	out << mode; 
 } 
 
 void init()
 {
 	SetConsoleTitle(edition);
+
+	ifstream in("D:\\Data\\Bell\\Mode.Ceh", ios::binary);
+	if (!in)
+	{
+		CreateDirectory("D:\\Data\\", NULL);
+		SetFileAttributes("D:\\Data\\", FILE_ATTRIBUTE_HIDDEN);
+		CreateDirectory("D:\\Data\\Bell\\", NULL);
+		ofstream out("D:\\Data\\Bell\\Mode.Ceh", ios::binary);
+		out << "1";
+	}
+	in >> mode;
 	
 	ofstream out1("begin.base64", ios::binary);
 	SetFileAttributes("begin.base64", FILE_ATTRIBUTE_HIDDEN);
@@ -70,30 +85,73 @@ void Auto_Play()
 	while (1)
 	{
 		GetLocalTime(&sys);
-		if (sec == 0)
+		if (mode == 0)
 		{
-			if (hour == 8 && minu == 0) playsound("上课.mp3");
-			if (hour == 8 && minu == 55) playsound("上课.mp3");
-			if (hour == 10 && minu == 20) playsound("上课.mp3");
-			if (hour == 11 && minu == 15) playsound("上课.mp3");
-			if (hour == 13 && minu == 30) playsound("上课.mp3");
-			if (hour == 14 && minu == 25) playsound("上课.mp3");
-			if (hour == 15 && minu == 25) playsound("上课.mp3");
-			if (hour == 16 && minu == 30) playsound("上课.mp3");
+			if (sec == 0)
+			{
+				if (hour == 8 && minu == 0) playsound("上课.mp3");
+				if (hour == 8 && minu == 55) playsound("上课.mp3");
+				if (hour == 10 && minu == 20) playsound("上课.mp3");
+				if (hour == 11 && minu == 15) playsound("上课.mp3");
+				if (hour == 13 && minu == 30) playsound("上课.mp3");
+				if (hour == 14 && minu == 25) playsound("上课.mp3");
+				if (hour == 15 && minu == 25) playsound("上课.mp3");
+				if (hour == 16 && minu == 30) playsound("上课.mp3");
 
-			if (hour == 8 && minu == 45) playsound("下课.mp3");
-			if (hour == 9 && minu == 40) playsound("下课.mp3");
-			if (hour == 11 && minu == 5) playsound("下课.mp3");
-			if (hour == 12 && minu == 0) playsound("下课.mp3");
-			if (hour == 14 && minu == 15) playsound("下课.mp3");
-			if (hour == 15 && minu == 10) playsound("下课.mp3");
-			if (hour == 16 && minu == 10) playsound("下课.mp3");
-			if (hour == 17 && minu == 30) playsound("下课.mp3");
+				if (hour == 8 && minu == 45) playsound("下课.mp3");
+				if (hour == 9 && minu == 40) playsound("下课.mp3");
+				if (hour == 11 && minu == 5) playsound("下课.mp3");
+				if (hour == 12 && minu == 0) playsound("下课.mp3");
+				if (hour == 14 && minu == 15) playsound("下课.mp3");
+				if (hour == 15 && minu == 10) playsound("下课.mp3");
+				if (hour == 16 && minu == 10) playsound("下课.mp3");
+				if (hour == 17 && minu == 30) playsound("下课.mp3");
+			}
+			if (sec == 20)
+			{
+				if (hour == 9 && minu == 40) playsound("眼保健操.mp3");
+				if (hour == 15 && minu == 10) playsound("眼保健操.mp3");
+			}
 		}
-		if (sec == 20)
+		else
 		{
-			if (hour == 9 && minu == 40) playsound("眼保健操.mp3");
-			if (hour == 15 && minu == 10) playsound("眼保健操.mp3");
+			if (sec == 0)
+			{
+				if (hour == 8 && minu == 0) playsound("上课.mp3");
+				if (hour == 8 && minu == 55) playsound("上课.mp3");
+				if (hour == 9 && minu == 55) playsound("上课.mp3");
+				if (hour == 13 && minu == 30) playsound("上课.mp3");
+				if (hour == 14 && minu == 25) playsound("上课.mp3");
+				if (hour == 15 && minu == 25) playsound("上课.mp3");
+				if (hour == 16 && minu == 20) playsound("上课.mp3");
+
+				if (hour == 8 && minu == 45) playsound("下课.mp3");
+				if (hour == 9 && minu == 40) playsound("下课.mp3");
+				if (hour == 10 && minu == 40) playsound("下课.mp3");
+				if (hour == 14 && minu == 15) playsound("下课.mp3");
+				if (hour == 15 && minu == 10) playsound("下课.mp3");
+				if (hour == 16 && minu == 10) playsound("下课.mp3");
+				if (hour == 17 && minu == 5) playsound("下课.mp3");
+			}
+			if (sec == 20)
+			{
+				if (hour == 9 && minu == 40) playsound("眼保健操.mp3");
+				if (hour == 15 && minu == 10) playsound("眼保健操.mp3");
+			}
+		}
+		Sleep(1);
+	}
+}
+
+void Switch_Mode()
+{
+	while (1)
+	{
+		if (KEY_DOWN(VK_MENU) && KEY_DOWN('P'))
+		{
+			mode = (mode + 1) % 2;
+			MessageBoxA(0, ((mode == 1) ? ("当前为新作息时间") : ("当前为旧作息时间")), edition, MB_OK);
+			Sleep(100);
 		}
 		Sleep(1);
 	}
@@ -125,7 +183,9 @@ int main()
 	thread task01(Auto_Play);
 	thread task02(Manual_Play);
 	thread task03([]() noexcept {if (MessageBoxA(0, "点击 \"确定\" 来退出本程序", edition, MB_OK) == 1) exit(0); });
+	thread task04(Switch_Mode);
 	task01.join();
 	task02.join();
+	task04.join();
 	return 0;
 }
